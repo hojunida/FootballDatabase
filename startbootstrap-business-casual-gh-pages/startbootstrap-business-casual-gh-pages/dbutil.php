296fb5fdbee42622d29d858785ec1552c953b179
Patrick Kwon <yk7da@virginia.edu>
2017-04-23 21:42:09 -0400

    dbutil 수정

--- startbootstrap-business-casual-gh-pages/startbootstrap-business-casual-gh-pages/dbutil.php
+++ startbootstrap-business-casual-gh-pages/startbootstrap-business-casual-gh-pages/dbutil.php
@@ -1,9 +1,9 @@
 <?php
 class DbUtil{
-	public static $loginUser = "cs4750s17ahy9ng"; 
+	public static $loginUser = "cs4750s17yk7daa"; 
 	public static $loginPass = "spring2017";
 	public static $host = "stardock.cs.virginia.edu"; // DB Host
-	public static $schema = "cs4750s17ahy9ng"; // DB Schema
+	public static $schema = "cs4750s17yk7da"; // DB Schema
 	
 	public static function loginConnection(){
 		$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
