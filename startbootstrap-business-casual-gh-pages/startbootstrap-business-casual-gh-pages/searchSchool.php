d7e88208635e65540efd639879046621e1a98612
Patrick Kwon <yk7da@virginia.edu>
2017-04-25 00:30:04 -0400

    positions

--- startbootstrap-business-casual-gh-pages/startbootstrap-business-casual-gh-pages/searchSchool.php
+++ startbootstrap-business-casual-gh-pages/startbootstrap-business-casual-gh-pages/searchSchool.php
@@ -8,9 +8,9 @@
 	if($stmt->prepare("select * from $searchString") or die(mysqli_error($db))) {
 		$stmt->execute();
 		$stmt->bind_result($No, $Name, $Pos, $Ht, $Wt, $Class, $Hometown, $State, $Team, $UID);
-		echo "<table border=1><th>No</th><th>Name</th><th>Pos</th><th>Ht</th><th>Wt</th><th>Class</th><th>Hometown</th><th>State</th><th>Team</th><th>UID</th>\n";
+		echo "<table border=1><th>No</th><th>Name</th><th>Ht</th><th>Wt</th><th>Class</th><th>Hometown</th><th>Pos</th><th>State</th><th>Team</th><th>UID</th><th>Check</th>\n";
 		while($stmt->fetch()) {
-			echo "<tr><td>$No</td><td>$Name</td><td>$Pos</td><td>$Ht</td><td>$Wt</td><td>$Class</td><td>$Hometown</td><td>$State</td><td>$Team</td><td>$UID</td></tr>";
+			echo "<tr><td>$No</td><td>$Name</td><td>$Ht</td><td>$Wt</td><td>$Class</td><td>$Hometown</td><td>$Pos</td><td>$State</td><td>$Team</td><td>$UID</td><td><input type='checkbox' onclick='getRow(this)' /></td></tr>";
 		}
 		echo "</table>";
 	
